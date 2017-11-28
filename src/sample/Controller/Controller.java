9c597e1322b481240a322fc5ff318142351c90bc
savchenkoto <lapital36@gmail.com>
2017-11-28 22:00:36 +0300

    First Nested Class

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -1,87 +1,16 @@
 package sample.Controller;
 
-import DAO.DaoImpl.TrainDaoImpl;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import model.Train;
+import javafx.scene.layout.AnchorPane;
 
-public class Controller {
 
-    private ObservableList<Train> trainList = FXCollections.observableArrayList();
+public class Controller extends AnchorPane {
 
     @FXML
-    private TableView<Train> trainTable;
+    private TrainController trainController;
 
-    @FXML
-    private TableColumn<Train, String> numberColumn;
-
-    @FXML
-    private TableColumn<Train, String> departureColumn;
-
-    @FXML
-    private TableColumn<Train, String> destinationColumn;
-
-    public Controller() {
-    }
+    public Controller() { }
 
 
-    @FXML
-    private void initialize() {
-        initData();
-
-        // устанавливаем тип и значение которое должно хранится в колонке
-        numberColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("number"));
-        departureColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("departure"));
-        destinationColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("destination"));
-
-        trainTable.setItems(trainList);
-    }
-
-    private void initData() {
-        TrainDaoImpl trainDao = new TrainDaoImpl();
-        trainList.addAll(trainDao.findAll());
-    }
-
-    @FXML
-    private TextField numberField;
-
-    @FXML
-    private TextField departureField;
-    @FXML
-    private TextField destinationField;
-
-    @FXML
-    public void addTrain(ActionEvent event) {
-
-        if (
-                (numberField.getText() == null || numberField.getText().trim().isEmpty()) ||
-                (departureField.getText() == null || departureField.getText().trim().isEmpty()) ||
-                (destinationField.getText() == null || destinationField.getText().trim().isEmpty())){
-            Alert error= new Alert(Alert.AlertType.INFORMATION);
-            error.setHeaderText("The data haven't added");
-            error.setContentText("All fields must be filled out");
-            error.showAndWait();
-        } else {
-            Train train = new Train(
-                    numberField.getText(),
-                    departureField.getText(),
-                    destinationField.getText()
-            );
-            TrainDaoImpl trainDao = new TrainDaoImpl();
-            trainDao.add(train);
-            trainList.add(train);
-
-            numberField.setText("");
-            departureField.setText("");
-            destinationField.setText("");
-        }
-    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,7 +2,6 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,10 +10,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("templates/sample.fxml")));
         primaryStage.setTitle("Tickets");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
